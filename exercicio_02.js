5eee6e8976710669ee15d4ce3a8a12ee5ffea541
mariany myllena <myllena.rasche@estudante.ifro.edu.br>
2025-04-24 09:39:00 -0400

    tarefa de sala

--- exercicio_02.js
+++ exercicio_02.js
@@ -1,3 +1,21 @@
-let Óla = 'Óla'
-console.log(Óla.toLowerCase())
-console.log(Óla.toUpperCase());
+// exercicio1
+let a = 8
+let b = 2
+console.log("soma = ",a+ b)
+console.log("subtsubtração = " ,a-b)
+console.log("multiplicação =" ,a*b)
+console.log("divisão =" ,a/b)
+
+// exercicio2
+let nome1 = 15
+let nome2 = 28
+console.log(nome1 == nome2);
+
+// exercicio3 
+let idade =15;
+let mensagem = idade >= 18? " maior de idade" : "menor de idade";
+console.log(mensagem);
+
+// exercicio4
+let num = 4
+let 
