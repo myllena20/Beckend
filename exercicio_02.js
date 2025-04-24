c3edf108d17951b9d777822c9d43e063f24d99b8
mariany myllena <myllena.rasche@estudante.ifro.edu.br>
2025-04-24 10:16:04 -0400

    correçao da atividade

--- exercicio_02.js
+++ exercicio_02.js
@@ -1,21 +1,36 @@
-// exercicio1
+// exercicio1 Crie um programa que declare duas variáveis e exiba o resultado da soma,
+//subtração, multiplicação e divisão desses números.
+
 let a = 8
 let b = 2
-console.log("soma = ",a+ b)
-console.log("subtsubtração = " ,a-b)
-console.log("multiplicação =" ,a*b)
-console.log("divisão =" ,a/b)
+let soma = a + b
+let subtsubtração = a - b
+let multiplicação = a * b
+let divisão = a / b
+console.log(`
+    ${a} + ${b} = ${soma}
+    ${a} - ${b} = ${subtsubtração}
+    ${a} * ${b} = ${multiplicação}
+    ${a} / ${b} = ${divisão}
+    `);
 
-// exercicio2
+// exercicio2  Declare duas idades e utilize operadores de comparação para verificar se uma
+//pessoa é mais velha que a outra
 let nome1 = 15
 let nome2 = 28
 console.log(nome1 == nome2);
 
-// exercicio3 
+// exercicio3 Crie uma variável idade com um número e use o operador ternário para verificar
+//se o valor armazenado na variável é maior ou igual a 18. Imprima “Maior de
+//idade” ou “Menor de idade”.
 let idade =15;
-let mensagem = idade >= 18? " maior de idade" : "menor de idade";
+let mensagem = idade >18? " maior de idade" : "menor de idade";
 console.log(mensagem);
 
-// exercicio4
-let num = 4
-let 
+// exercicio4 Crie uma variável com um número e use o operador ternário para verificar se é
+//par ou ímpar
+let e = 4
+let  soma = e % 2
+let men = e == 0 ? 'impar' : 'par'
+
+console.log(men)
