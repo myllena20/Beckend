61b36738aa248d759203dd801834923561209edd
myllena20 <myllena.rasche@estudante.ifro.edu.br>
2025-05-26 16:12:14 -0400

    exemplos

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+const carro = {
+    modelo: "corola"
+}
+
+function teste (valor){
+    valor = "valor alterado dendro da função";
+}
+
+let marca = "toyota"
+
+console.log(marca);
+teste(marca )
+console.log(marca);
